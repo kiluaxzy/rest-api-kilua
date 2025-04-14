264dffb8d4f500933a9d92283194eda28008c5eb
kilua <hapidkesya@gmail.com>
2025-04-14 13:54:47 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
 global.creator = 'Kiluaexc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.MONGO_DB_URI = "mongodb+srv://kilua:kiluaganteng12@cluster0.pzwnlhj.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "hapidganteng12" //isi apa aja bebas
 global.your_email = "pacarkheyy@gmail.com" //email
 global.email_password = "arpu eoim ypwo hcyi" //application password email
 global.limitCount = 10000
